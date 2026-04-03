0446400fab1b401c8229ad12cc6b8fc960d3ef05
Yasharth <yasharthsoubhari@gmail.com>
2026-04-03 23:48:05 +0530

    Installed the bcrypt,jsonwebtoken and socket io dependencies

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const env = {
   PORT: process.env.PORT || 3000,
   DB_URI: process.env.DB_URI || 'mongodb+srv://yt:battle@yt-complete-backend.89vupgt.mongodb.net/?appName=yt-complete-backend',
-  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_SECRET: process.env.JWT_SECRET || 'eleetebattle_super_secret_key_123',
+  NODE_ENV: process.env.NODE_ENV || 'development',
 };
 
 module.exports = env;
